a1087f334fb9d064f204322e770550d35cc05704
norhady1 <norhady1@gmail.com>
2023-12-06 17:40:40 +0100

    grab

--- AliProjekt/Assets/Nor/Scripts/SceneController.cs
+++ AliProjekt/Assets/Nor/Scripts/SceneController.cs
@@ -5,20 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void StartButton()
     {
         SceneManager.LoadScene("NyPlayer");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
