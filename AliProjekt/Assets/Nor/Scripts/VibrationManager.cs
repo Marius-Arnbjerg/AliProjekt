532245549648efc336cb54dc638137b49cbfccca
norhady1 <norhady1@gmail.com>
2023-12-06 21:06:55 +0100

    vibration

--- AliProjekt/Assets/Nor/Scripts/VibrationManager.cs
+++ AliProjekt/Assets/Nor/Scripts/VibrationManager.cs
@@ -1,11 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class VibrationManager : MonoBehaviour
 {
-    public void TriggerVibration(AudioClip vibrationAudio)
-    {
+    [Range(0,1)]
+    public float intensity;
+    public float duration;
 
+    private void Start()
+    {
+        XRBaseInteractable interactable = GetComponent<XRBaseInteractable>();
+        interactable.activated.AddListener(TriggerVibration);
+
+    }
+
+    public void TriggerVibration(BaseInteractionEventArgs eventArgs)
+    {
+        if(eventArgs.interactableObject is XRBaseControllerInteractor controllerInteractor)
+        {
+            TriggerVibration(controllerInteractor.xrController);
+        }
+    }
+    public void TriggerVibration(XRBaseController controller)
+    {
+        if (intensity > 0)
+            controller.SendHapticImpulse(intensity, duration);
     }
 }
