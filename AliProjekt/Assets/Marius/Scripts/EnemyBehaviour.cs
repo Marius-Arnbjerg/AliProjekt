0c7f96aee1252b5df166021e00e43d8e376a11bf
Marius-Arnbjerg <marius.arnbjerg@gmail.com>
2023-12-06 20:20:03 +0100

    Slow enemy

--- AliProjekt/Assets/Marius/Scripts/EnemyBehaviour.cs
+++ AliProjekt/Assets/Marius/Scripts/EnemyBehaviour.cs
@@ -8,10 +8,13 @@ public class EnemyBehaviour : MonoBehaviour
 	public Transform bulletOrigin;
 	public Transform player;
 
+	private Coroutine LookCoroutine;
+
 	public ParticleSystem gunShot;
 
 	private float nextFire;
 	public float fireRate = 2;
+	public float rotationSpeed = 1;
 
 	public LayerMask layers;
 	
@@ -30,10 +33,29 @@ public class EnemyBehaviour : MonoBehaviour
 	void Update()
 	{
 		CheckIfTimeToFire();
-
-		transform.LookAt(player);
 	}
 
+    private void FixedUpdate()
+    {
+		StartCoroutine(RotateTowardsPlayer());
+	}
+
+
+	private IEnumerator RotateTowardsPlayer()
+    {
+		Quaternion lookRotation = Quaternion.LookRotation(player.position - transform.position);
+
+		float time = 0;
+
+        while (time < 1)
+        {
+			transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, time);
+
+			time += Time.deltaTime * rotationSpeed;
+
+			yield return null;
+        }
+    }
 	void CheckIfTimeToFire()
 	{
 		if (Time.time > nextFire)
