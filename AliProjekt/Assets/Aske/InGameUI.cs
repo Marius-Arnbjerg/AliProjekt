6b2bf054bf1330cb2f82a23cf6967e0376de7328
akaise21 <94837345+akaise21@users.noreply.github.com>
2023-12-06 16:32:19 +0100

    SceneManager

--- AliProjekt/Assets/Aske/InGameUI.cs
+++ AliProjekt/Assets/Aske/InGameUI.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;                 
-using UnityEngine.SceneManagement;     
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 using TMPro;
 
 public class InGameUI : MonoBehaviour
 {
-    public float playerHealth = 100;
-    public float removeHealth = 1;
+    public int playerHealth = 100;
+    public int removeHealth = 1;
     public float countdownFloat = 60;
+    public int bulletCount = 6;
     //public float refillHealth = 25;
     //private float fullHealth = 100;
 
@@ -18,8 +20,12 @@ public class InGameUI : MonoBehaviour
     public TMP_Text countdownNumberText;
     public TMP_Text countdownText;
     public TMP_Text healthNumberText;
+    public TMP_Text bulletNumberText;
 
     public TMP_Text enemyHealthNumberText;
+
+    public GameObject pauseMenu;
+    private bool pausedGame = false;
 
     /*
     public Slider playerOneFuelSlider, playerTwoFuelSlider; 
@@ -29,10 +35,49 @@ public class InGameUI : MonoBehaviour
     public TMP_Text playerOneFuelText, playerTwoFuelText;
     */
 
+    public void PauseButtonPressed(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            PauseMenu();
+        }
+    }
+
+    public void PauseMenu()
+    {
+        if (pausedGame == false)
+        {
+            pauseMenu.SetActive(true);
+            pausedGame = true;
+            Time.timeScale = 0;
+        }
+        else if (pausedGame == true)
+        {
+            pauseMenu.SetActive(false);
+            pausedGame = false;
+            Time.timeScale = 1;
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
     public void RemoveHealthPlayer()                             
     {
         playerHealth = playerHealth - removeHealth;
-        healthNumberText.text = playerHealth.ToString("F0");
+        healthNumberText.text = playerHealth.ToString();
 
         if (playerHealth <= 0)                                        
             EmptyPlayerHealth();                                      
@@ -73,7 +118,7 @@ public class InGameUI : MonoBehaviour
     public void RemoveHealthEnemy()
     {
         enemyHealth = enemyHealth - removeHealth;
-        enemyHealthNumberText.text = enemyHealth.ToString("F0");
+        enemyHealthNumberText.text = enemyHealth.ToString();
 
         if (enemyHealth <= 0)
             EmptyPlayerHealth();
@@ -82,5 +127,15 @@ public class InGameUI : MonoBehaviour
     {
         Debug.Log("enemy ded");
         //game won
+    }
+    public void RemoveBullet()
+    {
+        bulletCount = bulletCount - removeHealth;
+        bulletNumberText.text = bulletCount.ToString();
+
+        if (bulletCount <= 0)
+        {
+            //empty bullets
+        }
     }
 }
